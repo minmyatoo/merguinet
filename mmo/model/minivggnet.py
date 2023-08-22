a8209fb4cac3dba16f76b8d691b04903e485e800
minmyatoo <myatviz@gmail.com>
2023-08-22 19:53:38 +0800

    add transfer learning

--- mmo/model/minivggnet.py
+++ mmo/model/minivggnet.py
@@ -1,62 +1,37 @@
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (
     BatchNormalization,
-    Conv2D,
-    MaxPooling2D,
-    Activation,
     Flatten,
     Dropout,
     Dense,
     Input,
 )
-from tensorflow.keras import backend as K
 
 class MerguiNet:
 
     @staticmethod
-    def _conv_block(x, filters, kernel_size, padding="same"):
-        x = Conv2D(filters, kernel_size, padding=padding)(x)
-        x = Activation("relu")(x)
-        x = BatchNormalization()(x)  # Use default axis (-1 for channels_last)
-        x = Conv2D(filters, kernel_size, padding=padding)(x)
-        x = Activation("relu")(x)
-        x = BatchNormalization()(x)
-        x = MaxPooling2D(pool_size=(2, 2))(x)
-        x = Dropout(0.25)(x)
-        return x
-
-    @staticmethod
-    def _fully_connected_block(x, units):
-        x = Dense(units)(x)
-        x = Activation("relu")(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.5)(x)
-        return x
-
-    @staticmethod
     def build(width, height, depth, classes):
         inputShape = (height, width, depth)
-        chanDim = -1 if K.image_data_format() == "channels_last" else 1
 
-        input_layer = Input(shape=inputShape)
-        x = input_layer
+        # Load the pre-trained VGG16 model without the top classification layers
+        base_model = VGG16(weights='imagenet', include_top=False, input_tensor=Input(shape=inputShape))
 
-        # Convolutional Blocks
-        x = MerguiNet._conv_block(x, 32, (3, 3))
-        x = MerguiNet._conv_block(x, 64, (3, 3))
-        x = MerguiNet._conv_block(x, 128, (3, 3))
-        x = MerguiNet._conv_block(x, 64, (3, 3))
-        x = MerguiNet._conv_block(x, 128, (3, 3))
-        x = MerguiNet._conv_block(x, 128, (3, 3))
+        # Freeze the weights of the pre-trained layers
+        for layer in base_model.layers:
+            layer.trainable = False
 
-        # Fully Connected Layers
+        x = base_model.output
+
+        # Add custom layers on top of the pre-trained model
         x = Flatten()(x)
-        x = MerguiNet._fully_connected_block(x, 512)
+        x = BatchNormalization()(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = BatchNormalization()(x)
+        x = Dense(classes, activation='softmax')(x)
 
-        # Output Layer
-        output_layer = Dense(classes, activation="softmax")(x)
-
-        model = Model(inputs=input_layer, outputs=output_layer)
+        model = Model(inputs=base_model.input, outputs=x)
 
         # Model Summary
         model.summary()
